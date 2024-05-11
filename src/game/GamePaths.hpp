0332bd886050e328d2f57b8c0dacbcf3a96e04e5
miladmazahri <miladmazahri1383@gmail.com>
2024-05-11 22:18:29 +0330

    all animation_png added and their path added to GamePaths.hpp file.

--- src/game/GamePaths.hpp
+++ src/game/GamePaths.hpp
@@ -12,16 +12,39 @@ const std::string WALL_NUT_CARD_IMAGE_PATH = "./assets/images/cards/wall-nut-car
 const std::string SELECT_ICON_IMAGE_PATH = "./assets/images/icon/select-icon.png";
 const std::string PLACE_PLANT_ICON_IMAGE_PATH = "./assets/images/icon/place-plant-icon.png";
 const std::string PEASHOOTER_PLANT_IDLE_ANIMATION_PATH = "./assets/images/plants/peashooter-idle-animation.png";
+const std::string PEASHOOTER_PLANT_SHOOTING_ANIMATION_PATH = "./assets/images/plants/peashooter-shooting-animation.png";
 const std::string SUNFLOWER_PLANT_IDLE_ANIMATION_PATH = "./assets/images/plants/sunflower-idle-animation.png";
+const std::string MELON_PLUT_PLANT_IDLE_ANIMATION_PATH = "./assets/images/plants/melon-plut-idle-animation.png";
+const std::string MELON_PLUT_PLANT_ATTACKING_ANIMATION_PATH = "./assets/images/plants/melon-plut-attacking-animation.png";
+const std::string SNOW_PEA_PLANT_IDLE_ANIMATION_PATH = "./assets/images/plants/snow-pea-idle-animation.png";
+const std::string SNOW_PEA_PLANT_SHOOTING_ANIMATION_PATH = "./assets/images/plants/snow-pea-shooting-animation.png";
+const std::string WALL_NUT_PLANT_IDLE_ANIMATION_PATH = "./assets/images/plants/wall-nut-idle-animation.png";
+const std::string WALL_NUT_PLANT_BREAKING_ANIMATION_PATH = "./assets/images/plants/wall-nut-breaking-animation.png";
+const std::string REGULAR_ZOMBIE_WALKING_ANIMATION_PATH = "./assets/images/zombies/zombie-walking-animation.png";
+const std::string REGULAR_ZOMBIE_ARMLESS_WALKING_ANIMATION_PATH = "./assets/images/zombies/walking-armless-zombie-animation.png";
+const std::string REGULAR_ZOMBIE_EATING_ANIMATION_PATH = "./assets/images/zombies/zombie-eating-animation.png";
+const std::string REGULAR_ZOMBIE_ARMLESS_EATING_ANIMATION_PATH = "./assets/images/zombies/armless-zombie-eating-animation.png";
+const std::string REGULAR_ZOMBIE_CUTTING_ARM_ANIMATION_PATH = "./assets/images/zombies/cutting-arm-zombie-animation.png";
+const std::string REGULAR_ZOMBIE_DYING_ANIMATION_PATH = "./assets/images/zombies/dying-zombie-animation.png";
+const std::string GARGANTUAR_ZOMBIE_WALKING_ANIMATION_PATH = "./assets/images/zombies/gargantuar-walking-animation.png";
+const std::string GARGANTUAR_ZOMBIE_ATTACKING_ANIMATION_PATH = "./assets/images/zombies/gargantuar-attacking-animation.png";
+const std::string GARGANTUAR_ZOMBIE_DYING_ANIMATION_PATH = "./assets/images/zombies/gargantuar-dying-animation.png";
+const std::string BUCKETHEAD_ZOMBIE_WALKING_ANIMATION_PATH = "./assets/images/zombies/walking-buckethead-zombie-animation.png";
+const std::string BUCKETHEAD_ZOMBIE_HURTING_ANIMATION_PATH = "./assets/images/zombies/hurting-buckethead-zombie-animation.png";
+const std::string BUCKETHEAD_ZOMBIE_HURTED_EATING_ANIMATION_PATH = "./assets/images/zombies/hurting-buckethead-zombie-eating-animation.png";
+const std::string BUCKETHEAD_ZOMBIE_EATING_ANIMATION_PATH = "./assets/images/zombies/buckethead-zombie-eating-animation.png";
+const std::string BUCKETHEAD_ZOMBIE_DYING_ANIMATION_PATH = "./assets/images/zombies/dying-buckethead-zombie-animation.png";
+const std::string LAWN_MOWER_ITEM_IDLE_ANIMATION_PATH = "./assets/images/items/lawn-mower-idle-animation.png";
+const std::string LAWN_MOWER_ITEM_STABLE_PIC_PATH = "./assets/images/items/lawn-mower-stable-pic.png";
+const std::string PEA_ITEM_SHOOTING_ANIMATION_PATH = "./assets/images/items/shooting-pea-animation.png";
+const std::string PEA_ITEM_SPLATS_ANIMATION_PATH = "./assets/images/items/pea-splats-animation.png";
+const std::string SNOWY_PEA_ITEM_SHOOTING_ANIMATION_PATH = "./assets/images/items/shooting-snowy-pea-animation.png";
+const std::string SNOWY_PEA_ITEM_SPLATS_ANIMATION_PATH = "./assets/images/items/snowy-pea-splats-animation.png";
+const std::string SUN_ITEM_MOVING_ANIMATION_PATH = "./assets/images/items/sun-move-animation.png";
+const std::string MELON_ITEM_SHOOTING_ANIMATION_PATH = "./assets/images/items/shooting-melon-animation.png";
+const std::string MELON_ITEM_SPLATS_ANIMATION_PATH = "./assets/images/items/melon-splats-animation.png";
 const std::string PLANTS_PROPERTIES_PATH = "./assets/properties/Plants.csv";
 const std::string ZOMBIES_PROPERTIES_PATH = "./assets/properties/Zombies.csv";
 const std::string ITEMS_PROPERTIES_PATH = "./assets/properties/Items.csv";
-
-
-
-
-
-
-
 
 #endif // ASSETPATHS_H
